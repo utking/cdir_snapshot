4a70276b492f2a71201e2df3cbcf4c8861ba276d
UtkinG <utking@mail.ru>
2016-08-30 22:17:41 +1000

    feat: Add helper functions
    Added helper functions for logging, creating and freeing listing nodes.
    Added a listing node structure for the linked list containing a
    directory entries. Added a function that writes a listing for a
    directory. Added a function checking if an entry is a directory.

--- snapshot.c
+++ snapshot.c
@@ -5,10 +5,104 @@ void printUsage(const char * executableName) {
 }
 
 void processDirectory(const char *dirPath) {
-	;
+	if (isDirectory(dirPath)) {
+		DIR *dir;
+		ListingNode *listing = NULL, 
+								*top = NULL;
+		struct dirent *dirEntry;
+		dir = opendir(dirPath);
+		if (dir) {
+			while ((dirEntry = readdir(dir))) {
+				if (!strncmp(dirEntry->d_name, ".", 256) || 
+						!strncmp(dirEntry->d_name, "..", 256)) {
+					continue;
+				}
+				printLog(LOG_INFO, dirEntry->d_name, 0);
+				if (!listing) {
+					listing = createNode(dirEntry->d_name);
+					top = listing;
+				} else {
+					listing->next = createNode(dirEntry->d_name);
+					listing = listing->next;
+				}
+				// If a current entry is directory, processDirectory(curEntry)
+			}
+			// writeListing(dirPath + "dir.lst")
+		}
+		while (top) {
+			//del cur node
+			break;
+		}
+	}
 }
 
-int writeListing(const char * listingFileName, const ListingNode * listing) {
+void printLog(enum LogType type, const char * msg, int errCode) {
+	switch (type) {
+		case LOG_ERR:
+			printf("Error (%d): %s [%s]\n", errCode, msg, strerror(errCode));
+			break;
+		case LOG_INFO:
+			printf("Info: %s\n", msg);
+			break;
+		case LOG_LOG:
+			printf("Log: %s\n", msg);
+			break;
+		default:
+			printf("%s\n", msg);
+	}
+}
+
+int writeListing(const char * listingFileName, ListingNode * listing) {
+	FILE *fd = fopen(listingFileName, "w");
+	ListingNode *top = listing;
+	unsigned int bytesWritten = 0;
+	char buf[256];
+	if (fd) {
+		/* write data */
+		while (top) {
+			memset(buf, 0, 256);
+			strncpy(buf, top->fileName, 255);
+			buf[strlen(buf)] = '\n';
+			bytesWritten = fwrite(buf, sizeof(char), strlen(buf), fd);
+			if (bytesWritten != strlen(buf)) {
+				printLog(LOG_ERR, "Can't write buffer", errno);
+			}
+			top = top->next;
+		}
+		printLog(LOG_LOG, "Done!", 0);
+		fclose(fd);
+		return 1;
+	} else {
+		printLog(LOG_ERR, "Can't write listing", 0);
+		return 0;
+	}
+}
+
+int isDirectory(const char* filePath) {
+#ifdef _WIN32
+	printLog(LOG_LOG, "Windows isDirectory", 0);
+#endif
+
+#ifdef __linux__
+	printLog(LOG_LOG, "Linux isDirectory", 0);
+	struct stat sb;
+	return (stat(filePath, &sb) == 0 && S_ISDIR(sb.st_mode));
+#endif
 	return 0;
+}
+
+ListingNode * createNode(const char * fileName) {
+	ListingNode * node = malloc(sizeof(ListingNode));
+	node->fileName = malloc(sizeof(char) * 256);
+	memset(node->fileName, 0, 256);
+	strncpy(node->fileName, fileName, 255);
+	node->next = NULL;
+
+	return node;
+}
+
+void freeNode(ListingNode *node) {
+	free(node->fileName);
+	free(node);
 }
 
--- snapshot.h
+++ snapshot.h
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <error.h>
+#include <errno.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#ifdef __linux__
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h> 
+#endif
 
 typedef struct _ListingNode {
 	char * fileName;
 	struct _ListingNode * next;
 } ListingNode;
 
-void printUsage(const char * executableName);
-void processDirectory(const char *dirPath);
-int writeListing(const char * listingFileName, const ListingNode * listing);
+
+enum LogType { LOG_ERR, LOG_INFO, LOG_LOG };
+
+ListingNode * createNode(const char*);
+void freeNode(ListingNode *);
+void printLog(enum LogType, const char*, int);
+void printUsage(const char*);
+void processDirectory(const char*);
+int writeListing(const char*, ListingNode*);
+int isDirectory(const char*);
