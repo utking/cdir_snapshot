bfabf77b98d2073a00d04cd254453a23e70053ff
UtkinG <utking@mail.ru>
2016-08-30 22:14:51 +1000

    feat: Change main.c file to start the process
    main.c starts the process that will continue within snapshot.c

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include "snapshot.h"
 
 int main(int argc, char** argv) {
-	printf("Directory snapshot\n");
-	printUsage(argv[0]);
+	char rootDirPath[256];
+	
+	if (argc != 2) {
+		printUsage(argv[0]);
+		return 0;
+	}
+
+	memset(rootDirPath, 0, 256);
+	strncpy(rootDirPath, argv[1], 255);
+	
+	processDirectory(rootDirPath);
+	printLog(LOG_INFO, "Completed", 0);
 
 	return 0;
 }
