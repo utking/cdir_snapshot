5192b07bfa7f0238e7c27fd98a2d79cf13f8270b
UtkinG <utking@mail.ru>
2016-10-01 22:07:47 +1000

    fix: Fix printUsage() with the "process hidden" argument

--- snapshot.c
+++ snapshot.c
@@ -21,6 +21,7 @@ ListingNode * singleListing = NULL;
 void printUsage(const char * executableName) {
   printf("Usage: %s %s\n", executableName, "<directory path> [-sqh] [-d <D>] [-f <F>] [-l <dir.lst>]");
   printf("Options:\n");
+  printf("\t-a - process hidden files. Disabled by default.\n");
   printf("\t-s - single listing. Save items in a single file in the current directory\n");
   printf("\t-d - set a custom directory prefix letter. 'D' by default.\n");
   printf("\t-f - set a custom file prefix letter. 'F' by default.\n");
